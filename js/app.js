da3cf04df1cc58b78f7e0d2a4ca623ba4d9649af
Nurul Islam <nurul.islam96@aol.com>
2022-08-23 04:01:38 +0600

    calculate section done

--- js/app.js
+++ js/app.js
@@ -1,26 +1,55 @@
-//Selected-v Section
+// Selected-v Section
 const selectButton = document.getElementsByClassName('select-btn');
 for (let i = 0; i < selectButton.length; i++) {
     selectButton[i].addEventListener("click", function () {
         const playersName = document.getElementsByClassName('player-name');
         const playerIndividual = playersName[i].innerText;
         const playerList = document.getElementById('player-list');
+        if(playerList.)
         let newPlayer = document.createElement('li');
         newPlayer.textContent = playerIndividual;
         playerList.appendChild(newPlayer);
         selectButton[i].disabled = true;
+        
     })
 }
 
-
-//Common function to get input value
-function getInputValueById(value){
+//function to get input value
+function getInputValueById(value) {
     const inputField = document.getElementById(value);
     const inputFieldValue = inputField.value;
+    // inputField.value = '';
     return inputFieldValue;
 }
 
-//Calcute Total Cost
+function calculateExpense(){
+    const perPlayerCost = getInputValueById('player-cost');
+    const totalExpense = perPlayerCost * 5;
+    return totalExpense;
+}
+function calculateTotal(){
+    const managerCost = parseFloat(getInputValueById('manager-cost'));
+    const coachCost = parseFloat(getInputValueById('coach-cost'));
+    const totalCost = calculateExpense() + managerCost + coachCost;
+    return totalCost;
+}
+
+//calculate expense button
+
+document.getElementById('calculate-btn').addEventListener('click', function(){
+    const previousExpense = document.getElementById('previous-expense');
+    previousExpense.innerText = calculateExpense();
+})
+
+document.getElementById('total-button').addEventListener('click', function(){
+    const previousTotalCost = document.getElementById('total-expense');
+    previousTotalCost.innerText = calculateTotal();
+})
+
+
+
+
+
 
 
 
